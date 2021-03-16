d82ac8c3a32086114d6d6333c6fc8a10ba7d1279
Sahil Kumar <73190412+thesahil18@users.noreply.github.com>
2021-03-16 21:33:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+'''
+There will be 1 year differnce in this program.
+If you cross check check yor age in real or through the program.
+If You check your age from present age input then there also 1 year differnce in present input or abirth input.
+'''
+print('''----------------AGE CALCULATOR----------------\n\n''')
+
+present_year = int(input("Enter Present Year : "))
+future_year = int(input("Enter Year to check your future Age : "))
+    
+print('''1. Enter Your Age\n2. Enter Your Birth year\n''')
+
+choice = int(input("Enter Your Choice : "))
+    
+if choice == 1:
+    age_year = int(input("Enter Your Present Age : "))
+        
+    if age_year == 0 and len(str(age_year)) == 1:
+        print(f'Your age in {future_year} is : {future_year - present_year}')
+
+    elif age_year > 0 and len(str(age_year))>=1 and len(str(age_year))<=3:
+        print(f'Your age in {future_year} is : {future_year-present_year+age_year}')
+            
+    else:
+        print("<<<Invalid Age>>>")
+elif choice == 2:
+    age_year = int(input("Enter Your Birth Year : "))
+    if age_year > present_year:
+        print("<<<Invalid Input>>>")
+    else:
+        if future_year < present_year:
+            print("<<<Invalid Input>>>")
+        else:
+            print(f'Your age in {future_year} is : {(present_year-age_year)+(future_year-present_year)}')            
+else:
+    print("<<<Invalid Input>>>")
